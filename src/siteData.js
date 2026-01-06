a6875dd6ccef57fd1589d08a6bab7fcfa0d3772e
Anton Staroverov <5911853+73071319+tonystar@users.noreply.github.com>
2026-01-06 02:44:37 -0600

    Add site.isProd global data to Eleventy plugin, indicating production mode; enhance site.year data availability in templates.

--- src/siteData.js
+++ src/siteData.js
@@ -1,10 +1,12 @@
 /**
- * Add site.year global data
- * Sets the current year to be available in all templates as {{ site.year }}
+ * Add site.year and site.isProd global data
+ * - site.isProd: Boolean indicating if running in production mode (build) vs development (serve)
+ * - site.year: Sets the current year to be available in all templates as {{ site.year }}
  * 
  * @param {Object} eleventyConfig - The Eleventy configuration object
  */
 export function siteData(eleventyConfig) {
+  eleventyConfig.addGlobalData("site.isProd", () => process.env.ELEVENTY_RUN_MODE === "build");
   eleventyConfig.addGlobalData("site.year", () => new Date().getFullYear());
 }
 
